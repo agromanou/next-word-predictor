ae1969a4d3f2719a17c0e975c191ffc313f62cef
giorgos <perakisgeorgios@gmail.com>
2018-01-28 16:18:52 +0200

    created dummy method for sentences extraction and train_dev_test splits.

--- load_data.py
+++ load_data.py
@@ -1,8 +1,57 @@
 import os
-import nltk
-from numpy.random import choice
+
+import numpy as np
+import pandas as pd
+from numpy import random
 
 DATA_DIR = "{}{}{}{}".format(os.getcwd(), os.sep, 'data', os.sep)
+
+
+def split_to_sentences(data):
+    """
+
+    :param data:
+    :return:
+    """
+    sentences = data.split('.')
+
+    return sentences
+
+
+def split_in_train_dev_dest(sentences, seed=1234, dev_size=0.20, test_size=.10, save_datasets=False):
+    """
+
+    :param sentences:
+    :param seed:
+    :param dev_size:
+    :param test_size:
+    :return:
+    """
+    # setting the seed in order to be able to reproduce results.
+    np.random.seed(seed)
+
+    # shuffling the list of sentences.
+    random.shuffle(sentences)
+
+    total_len = len(sentences)
+    dev_sentences_size = int(total_len * dev_size)
+    test_sentences_size = int(total_len * test_size)
+    train_sentences_size = total_len - dev_sentences_size - test_sentences_size
+
+    train_data = sentences[:train_sentences_size]
+    dev_data = sentences[train_sentences_size:train_sentences_size + dev_sentences_size]
+    test_data = sentences[train_sentences_size + dev_sentences_size:]
+
+    if save_datasets:
+        train_df = pd.DataFrame(train_data, columns=['text'])
+        dev_df = pd.DataFrame(dev_data, columns=['text'])
+        test_df = pd.DataFrame(test_data, columns=['text'])
+
+        train_df.to_csv(DATA_DIR + 'europarl_train.csv', encoding='utf-8', index=False)
+        dev_df.to_csv(DATA_DIR + 'europarl_dev.csv', encoding='utf-8', index=False)
+        test_df.to_csv(DATA_DIR + 'europarl_test.csv', encoding='utf-8', index=False)
+
+    return train_data, dev_data, test_data
 
 
 def load_dataset():
@@ -20,7 +69,10 @@ def load_dataset():
 
 if __name__ == "__main__":
 
-    data = load_dataset()
+    en_data = load_dataset()
 
-    sentences = data.split('.')
-    print(len(sentences))
+    en_sentences = split_to_sentences(en_data)
+    train, dev, test = split_in_train_dev_dest(en_sentences)
+
+    for sentence in train[:10]:
+        print(sentence.strip(), end='\n\n')
