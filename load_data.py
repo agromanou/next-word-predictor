a2cdfa62cf1bae151abc853acb8ac8a77fafe6b3
giorgos <perakisgeorgios@gmail.com>
2018-01-28 15:13:28 +0200

    load english file.

--- load_data.py
+++ load_data.py
@@ -2,8 +2,8 @@ import os
 import nltk
 
 DATA_DIR = "{}{}{}{}".format(os.getcwd(), os.sep, 'data', os.sep)
-infile = DATA_DIR + 'europarl-v7.el-en.en'
 
+infile = DATA_DIR + 'europarl-v7.el-en.en'
 
 with open(infile, 'rb') as in_file:
     data = in_file.read()
