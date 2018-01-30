689d9bca4e79f7af04352d27355d3b2fb2ffafcc
aggrom <agromanou@gmail.com>
2018-01-31 00:24:10 +0200

    Implement tests for create_ngrams()

--- Assignment1/test/unit/text_utils_test.py
+++ Assignment1/test/unit/text_utils_test.py
@@ -1,8 +1,6 @@
 from Assignment1.text_utils import TextUtils
 
 import unittest
-import pandas as pd
-import numpy as np
 
 
 class TextUtilsTest(unittest.TestCase):
@@ -16,22 +14,33 @@ class TextUtilsTest(unittest.TestCase):
     def test_create_ngrams_with_2_n(self):
         sentence = "This is a quite large sentence"
         exp_outcome = [['This', 'is'],
-                   ['is', 'a'],
-                   ['a', 'quite'],
-                   ['quite', 'large'],
-                   ['large', 'sentence']]
+                       ['is', 'a'],
+                       ['a', 'quite'],
+                       ['quite', 'large'],
+                       ['large', 'sentence']]
         print(sentence)
 
         ngrams = self.theTextUnitObject.create_ngrams(sentence.split(), 2)
-
-        for ngram in self.theTextUnitObject.create_ngrams(sentence.split(), 2):
-            print(ngram)
 
         self.assertEqual(len(ngrams), 5)
         self.assertEqual(type(ngrams), type(exp_outcome))
         self.assertListEqual(ngrams, exp_outcome)
 
+    def test_create_ngrams_with_3_n(self):
+        sentence = "This is a quite large sentence"
+        exp_outcome = [['This', 'is', 'a'],
+                       ['is', 'a', 'quite'],
+                       ['a', 'quite', 'large'],
+                       ['quite', 'large', 'sentence']]
+        print(sentence)
 
+        ngrams = self.theTextUnitObject.create_ngrams(sentence.split(), 3)
 
+        self.assertEqual(len(ngrams), 4)
+        self.assertEqual(type(ngrams), type(exp_outcome))
+        self.assertListEqual(ngrams, exp_outcome)
 
-
+    def test_create_ngram_with_invalid_n(self):
+        sentence = ""
+        self.assertRaises(AssertionError, self.theTextUnitObject.create_ngrams, sentence.split, 1)
+        self.assertRaises(AssertionError, self.theTextUnitObject.create_ngrams, sentence.split, 5)
