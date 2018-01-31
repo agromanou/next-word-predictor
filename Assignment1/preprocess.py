bf35951f7bc6756fd78f439ce2d72aced5617d1a
Perakis Georgios <perakisgeorgios@gmail.com>
2018-01-31 10:58:13 +0200

    Created calculate_ngram_counts() method.

--- Assignment1/preprocess.py
+++ Assignment1/preprocess.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from pprint import pprint
 
 
 class Preprocessor(object):
@@ -7,13 +8,13 @@ class Preprocessor(object):
         self.vocabulary = None
 
     @staticmethod
-    def split_to_sentences(data):
+    def split_to_sentences(corpus):
         """
 
-        :param data:
+        :param corpus:
         :return:
         """
-        sentences = list(map(lambda s: s.strip(), data.split('.')))
+        sentences = list(map(lambda s: s.strip(), corpus.split('.')))
         filtered = list(filter(lambda x: x != '', sentences))
 
         return filtered
@@ -66,10 +67,60 @@ class Preprocessor(object):
         :param n: The length of word tuples
         :return: The n-grams for a given sentence.
         """
-        assert n in [2, 3]
+        assert n in [1, 2, 3]
 
         return [seq[i:i + n] for i in range(len(seq) - n + 1)]
 
+    def calculate_ngram_counts(self, corpus, model):
+        """
+        This method calculates all the n-gram counts for a given model.
+        :param corpus: Str. A text
+        :param model: Str. Enum defining the model that we want to use.
+        :return: Counter. A python Counter of the word n-grams.
+        """
+
+        assert model in ['bigram', 'trigram']
+
+        model_n = 2 if model == 'bigram' else 3
+
+        all_ngrams = list()
+
+        # splitting corpus into sentences
+        sentences = self.split_to_sentences(corpus)
+
+        # padding each sentence separately in respect to the given model
+        padded_sentences = tuple(map(lambda s: self.tokenize_and_pad(s, model_type=model), sentences))
+
+        # calculates all the n-grams up to the models actual n.
+        # E.g for trigram, creates uni-grams, bi-grams and tri-grams
+        for num in range(1, model_n + 1):
+
+            sentences_ngrams = map(lambda s: self.create_ngrams(s, n=num), padded_sentences)
+
+            # appends each n-gram into a list in order to count them.
+            for sublist in sentences_ngrams:
+                for item in sublist:
+                    all_ngrams.append(" ".join(item))
+
+        # counting all the different n-grams.
+        return Counter(all_ngrams)
+
 
 if __name__ == '__main__':
-    pass
+
+    a_corpus = "The Cape sparrow (Passer melanurus) is a southern African bird. A medium-sized sparrow at 14–16 " \
+               "centimetres (5.5–6.3 in), it has distinctive grey, brown, and chestnut plumage, with large pale head " \
+               "stripes in both sexes. The male has some bold black and white markings on its head and neck. The " \
+               "species inhabits semi-arid savannah, cultivated areas, and towns, from the central coast of Angola to " \ 
+               "eastern South Africa and Swaziland. Cape sparrows primarily eat seeds, along with soft plant parts " \
+               "and insects. They typically breed in colonies, and search for food in large nomadic flocks. The nest " \
+               "can be constructed in a tree, bush, cavity, or disused nest of another species. A typical clutch " \
+               "contains three or four eggs, and both parents are involved, from nest building to feeding the young. " \
+               "The species is common in most of its range and coexists successfully in urban habitats with two of " \
+               "its relatives, the native southern grey-headed sparrow and the house sparrow, an introduced species. " \
+               "The Cape sparrow's population has not decreased significantly, and is not seriously threatened by " \
+               "human activities. "
+
+    counts = Preprocessor().calculate_ngram_counts(corpus=a_corpus, model='bigram')
+
+    pprint(counts)
