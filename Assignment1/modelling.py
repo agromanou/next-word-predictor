00207abc6947c1bb2565adba86cc34a318c106d1
aggrom <agromanou@gmail.com>
2018-02-04 14:33:04 +0200

    Implement fit_model() that runs all the modeling process (all Model's methods)

--- Assignment1/modelling.py
+++ Assignment1/modelling.py
@@ -1,17 +1,37 @@
 import numpy as np
 
+
 class Model(object):
     def __init__(self,
-                 smoothing_algo,
                  vocabulary,
-                 tokens):
+                 tokens,
+                 ngrams):
 
-        assert smoothing_algo in ['laplace_smoothing', 'k_n']
-
-        self.smoothing_algo = smoothing_algo
         self.vocabulary = vocabulary
         self.tokens = tokens
-        self.prob = {}
+        self.ngrams = ngrams
+        self.probs = {}
+        self.smoothed_probs = {}
+        self.log_probs = {}
+
+    def fit_model(self, smoothing_algo):
+        """
+
+        :param smoothing_algo:
+        :return:
+        """
+        assert smoothing_algo in ['laplace_smoothing', 'k_n']
+
+        self.probs = self.calculate_bayes_probs(self.ngrams, self.vocabulary)
+        self.perform_smoothing(smoothing_algo)
+        self.log_prob()
+
+        print(self.vocabulary)
+        print(len(self.vocabulary))
+        print(self.ngrams)
+        print(self.probs)
+        print(self.smoothed_probs)
+        print(self.log_probs)
 
     @staticmethod
     def calculate_bayes_probs(grams, voc):
@@ -21,22 +41,22 @@ class Model(object):
         :param tokens:
         :return:
         """
-        return dict(map(lambda p: (p, grams[p] / voc[p.split()[0]]), grams))
+        return dict(map(lambda p: (p, grams[p] / voc[p[0]]), grams))
 
-    def perform_smoothing(self):
+    def perform_smoothing(self, smoothing_algo):
         """
 
         :return:
         """
-        if self.smoothing_algo == "laplace_smoothing":
+        if smoothing_algo == "laplace_smoothing":
 
             print("Running Laplace smoothing process..")
-            self.prob = self.laplace_smoothing()
+            self.smoothed_probs = self.laplace_smoothing()
 
-        elif self.smoothing_algo == "k_n":
+        elif smoothing_algo == "k_n":
 
             print("Running Kneser-Ney smoothing process..")
-            self.prob = self.kneser_ney_smoothing()
+            self.smoothed_probs = self.kneser_ney_smoothing()
         else:
             print("Please choose a valid smoothing method.")
 
@@ -48,7 +68,7 @@ class Model(object):
         """
         pl = dict(
             map(lambda c: (c[0], (c[1] + add_k) / (len(self.tokens) + add_k * len(self.vocabulary))),
-                      self.vocabulary.items()))
+                self.probs.items()))
         return pl
 
     def kneser_ney_smoothing(self):
@@ -56,22 +76,21 @@ class Model(object):
 
         :return:
         """
-        pass
+        return self.probs
 
     def log_prob(self):
         """
 
         :return: a dictionary with n-grams and assigned log probabilities
         """
-        log_prob = dict(map(lambda k: (k, np.log(self.prob[k])), self.prob))
-        return log_prob
+        log_prob = dict(map(lambda k: (k, - np.log(self.smoothed_probs[k])), self.smoothed_probs))
+        self.log_probs = log_prob
 
     def mle(self):
         pass
 
 
 if __name__ == '__main__':
-
     tokens = ["i", "want", "to", "eat", "chinese", "food", "lunch", "spend",
               "i", "want", "to", "eat", "chinese", "food", "lunch", "spend",
               "i", "want", "to", "eat", "chinese", "food", "lunch", "spend",
@@ -86,22 +105,16 @@ if __name__ == '__main__':
                        "lunch": 3,
                        "spend": 3}
 
-    ngrams = {'i want': 4,
-              'want to': 4,
-              'to eat': 4,
-              'eat chinese': 3,
-              'chinese food': 3,
-              'food lunch': 3,
-              'lunch spend': 3}
+    ngrams = {('i', 'want'): 4,
+              ('want', 'to'): 4,
+              ('to', 'eat'): 4,
+              ('eat', 'chinese'): 3,
+              ('chinese', 'food'): 3,
+              ('food', 'lunch'): 3,
+              ('lunch', 'spend'): 3}
 
-    modelObj = Model("laplace_smoothing",
-                     vocabulary_freq,
-                     tokens)
-    modelObj.perform_smoothing()
-    print(modelObj.prob)
+    modelObj = Model(vocabulary_freq,
+                     tokens,
+                     ngrams)
 
-    probs = Model("laplace_smoothing",
-                  vocabulary_freq,
-                  tokens).calculate_bayes_probs(ngrams,
-                                                vocabulary_freq)
-    print(probs)
+    modelObj.fit_model("laplace_smoothing")
