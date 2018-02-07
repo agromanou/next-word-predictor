4c8ee3c3ef900dfa160b597a8bf20c79e94dc709
Perakis Georgios <perakisgeorgios@gmail.com>
2018-02-07 10:21:51 +0200

    added logs to data_fetcher.py

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,38 +1,38 @@
-from Assignment1.preprocess import Preprocessor
 from Assignment1.data_fetcher import Fetcher
+from Assignment1.preprocess import Preprocessor
 
 if __name__ == '__main__':
 
-    pp = Preprocessor()
+    # pp = Preprocessor()
+    #
+    # dl = Fetcher(file='europarl-v7.el-en.',
+    #              language='en')
+    #
+    # # Load data & split sentences
+    # en_data = dl.load_dataset()
+    # en_sentences = pp.split_to_sentences(en_data)
+    # print(len(en_sentences))
+    #
+    # # Split data into train, dev and test
+    # dl.split_in_train_dev_test(en_sentences,
+    #                            save_data=False)
 
-    dl = Fetcher(file='europarl-v7.el-en.',
-                 language='en')
+    # train, dev, test = dl.train_data, dl.dev_data, dl.test_data
 
-    # Load data & split sentences
-    en_data = dl.load_dataset()
-    en_sentences = pp.split_to_sentences(en_data)
+    # # Tokenize sentences
+    # res = pp.tokenize_and_pad(a_sentence)
+    #
+    # # Create vocabulary
+    # counts = pp.create_vocabulary(some_sentences)
+    #
+    # # Create n-grams
+    # for i in pp.create_ngrams(a_sentence.split(), 3):
+    #     print(i)
 
-    # Split data into train, dev and test
-    dl.split_in_train_dev_test(en_sentences,
-                               save_data=True)
+    a_sentence = "This is a huge sentence that I want to exclude many different words for some reason"
 
-    train, dev, test = dl.train_data, dl.dev_data, dl.test_data
+    import re
 
-    for sentence in train[:10]:
-        print(sentence.strip(), end='\n\n')
+    out = a_sentence.replace('reason|wer', 'AAAA')
 
-    a_sentence = "This is a quite large sentence"
-
-    some_sentences = ["This is a sentence",
-                      "This is another sentence",
-                      "This is new fucking awesome sentence"]
-
-    # Tokenize sentences
-    res = pp.tokenize_and_pad(a_sentence)
-
-    # Create vocabulary
-    counts = pp.create_vocabulary(some_sentences)
-
-    # Create n-grams
-    for i in pp.create_ngrams(a_sentence.split(), 3):
-        print(i)
+    print(out)
